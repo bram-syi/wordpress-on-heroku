7b68eee2c5445952ab953c271a4ae9020295b305
Bram Whillock <bramski@gmail.com>
2013-05-29 12:40:37 -0700

    Need to see phpinfo.

--- config/public/phpinfo.php
+++ config/public/phpinfo.php
@@ -4,17 +4,17 @@
     exit();
   }
 
-  if (getenv("SYSTEM_USERNAME") == "" || getenv("SYSTEM_PASSWORD") == "") {
-    header("HTTP/1.0 403 Forbidden");
-    exit();
-  }
+  #if (getenv("SYSTEM_USERNAME") == "" || getenv("SYSTEM_PASSWORD") == "") {
+  #  header("HTTP/1.0 403 Forbidden");
+  #  exit();
+  #}
 
-  if ($_SERVER["PHP_AUTH_USER"] == getenv("SYSTEM_USERNAME") &&
-      $_SERVER["PHP_AUTH_PW"] == getenv("SYSTEM_PASSWORD")) {
+  #if ($_SERVER["PHP_AUTH_USER"] == getenv("SYSTEM_USERNAME") &&
+  #    $_SERVER["PHP_AUTH_PW"] == getenv("SYSTEM_PASSWORD")) {
     phpinfo();
-  } else {
-    header("WWW-Authenticate: Basic realm='System access'");
-    header("HTTP/1.0 401 Unauthorized");
-    print "Access denied!\n";
-  }
+  #} else {
+  #  header("WWW-Authenticate: Basic realm='System access'");
+  #  header("HTTP/1.0 401 Unauthorized");
+  #  print "Access denied!\n";
+  #}
 ?>
