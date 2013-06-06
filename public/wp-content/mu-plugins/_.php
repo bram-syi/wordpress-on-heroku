ca7481803548adee991a4f5a773400e766149656
Bram Whillock <bramski@gmail.com>
2013-06-06 15:12:49 -0700

    Adding translation for siteurl.

--- public/wp-content/mu-plugins/_.php
+++ public/wp-content/mu-plugins/_.php
@@ -69,6 +69,7 @@ add_action('sanitize_comment_cookies', 'stopwatch_hook');
 add_action('head', 'stopwatch_hook');
 add_filter("option_siteurl","syi_siteurl_filter");
 add_filter("option_home","syi_siteurl_filter");
+add_filter("home_url","syi_siteurl_filter");
 
 
 define('SYI_PATH', ABSPATH . 'syi/');
